d48fa4d434e6c5b599ba0d13b965d880c5324c41
JamesNix <jamesfnix@gmail.com>
2014-05-27 18:11:41 -0500

    Tests for blank email, wrong pw confirmation

--- spec/features/user_edit_account_spec.rb
+++ spec/features/user_edit_account_spec.rb
@@ -8,7 +8,6 @@ feature "User edits account" do
     fill_in "Email", with: "joe@example.com"
     fill_in "Password", with: "mypassword"
     click_button "Sign in"
-    # save_and_open_page
     visit '/users/edit'
     fill_in "Email", with: "elvis@example.com"
     fill_in "Password", with: "newpassword"
@@ -16,27 +15,61 @@ feature "User edits account" do
     fill_in "Current password", with: "mypassword"
     click_button "Update"
     page.should have_content "You updated your account successfully."
+
     page.should_not have_link("Update")
   end
 
-  scenario "failed account edit" do
+  scenario "user enters wrong password confirmation" do
     User.create(email: "joe@example.com", password: "mypassword")
     visit '/'
     click_link "Sign in"
     fill_in "Email", with: "joe@example.com"
     fill_in "Password", with: "mypassword"
     click_button "Sign in"
-    # save_and_open_page
     visit '/users/edit'
     fill_in "Email", with: "elvis@example.com"
     fill_in "Password", with: "newpassword"
     fill_in "Password confirmation", with: "notnewpassword"
     fill_in "Current password", with: "mypassword"
     click_button "Update"
-    page.should have_content "Password confirmationdoesn't match Password"
-    # page.should_not have_link("Update")
+    page.should have_content "Please review the problems below:"
 
-    # page.should have_error("has already been taken", on: "Email")
     page.should have_error("doesn't match Password", on: "Password confirmation")
+  end
+
+  scenario "user enters wrong current password" do
+    User.create(email: "joe@example.com", password: "mypassword")
+    visit '/'
+    click_link "Sign in"
+    fill_in "Email", with: "joe@example.com"
+    fill_in "Password", with: "mypassword"
+    click_button "Sign in"
+    visit '/users/edit'
+    fill_in "Email", with: "elvis@example.com"
+    fill_in "Password", with: "newpassword"
+    fill_in "Password confirmation", with: "newpassword"
+    fill_in "Current password", with: "notmypassword"
+    click_button "Update"
+    page.should have_content "Please review the problems below:"
+
+    page.should have_error("is invalid", on: "Current password")
+  end
+
+  scenario "user leaves email blank" do
+    User.create(email: "joe@example.com", password: "mypassword")
+    visit '/'
+    click_link "Sign in"
+    fill_in "Email", with: "joe@example.com"
+    fill_in "Password", with: "mypassword"
+    click_button "Sign in"
+    visit '/users/edit'
+    fill_in "Email", with: ""
+    fill_in "Password", with: "newpassword"
+    fill_in "Password confirmation", with: "newpassword"
+    fill_in "Current password", with: "mypassword"
+    click_button "Update"
+    page.should have_content "Please review the problems below:"
+
+    page.should have_error("can't be blank", on: "Email")
   end
 end

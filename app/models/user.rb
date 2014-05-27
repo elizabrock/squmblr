5c958f0a95f474af3e686361e2a1d2f8f54a0d07
Adam Thede <adam@thedetech.com>
2014-05-27 16:42:26 -0500

    successful implementation of devise and omniauth/github :octopus:

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
 
   validates :username, uniqueness: true
 
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
+  def self.find_or_create_for_github_oauth(auth)
+    auth_token = auth.credentials.token
+    user = User.where(uid: auth.uid).first_or_create do |user|
+      user.uid = auth.uid
+      user.provider = auth.provider
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.name
+      # user.gravatar = auth.extra.raw_info.gravatar_id
     end
+    # unless user.github_access_token == auth_token
+    #   user.update_attribute(:github_access_token, auth_token)
+    # end
+    user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-
-  devise_for :users
   root to: "home#index"
+
+  devise_for :users, controllers: { omniauth_callbacks: :omniauth_callbacks }
+
+  # devise_scope :user do
+  #  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  #  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

3fd060f128b80592d8bc294e6106415fbf047f38
rtmalone <rtylermalone@gmail.com>
2014-05-27 14:09:28 -0500

    Posts can be created and index viewed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :posts
   root to: "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.

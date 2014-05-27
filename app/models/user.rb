a82001be14cdee0296326313134a0b3a948b4248
Robert Fryman <robert.fryman@gmail.com>
2014-05-27 15:49:20 -0500

    rails g devise:views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :recoverable, :trackable, :validatable
 
   validates :username, uniqueness: true
 end
---
+++ spec/features/user_password_recovery_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+feature "User recovers password" do
+  scenario "happy path email" do
+    User.create(email: "joe@example.com", username:"joe", password:"password", password_confirmation:"password")
+    visit '/'
+    click_link "Sign in"
+    click_link "Forgot your password?"
+    fill_in "user_email", with: "joe@example.com"
+    click_button "Send me reset password instructions"
+    page.should have_content "You will receive an email with instructions on how to reset your password in a few minutes."
+  end
+  scenario "happy path username" do
+    pending
+    User.create(email: "joe@example.com", username:"joe", password:"password", password_confirmation:"password")
+    visit '/'
+    click_link "Sign in"
+    click_link "Forgot your password?"
+    fill_in "user_email", with: "joe"
+    click_button "Send me reset password instructions"
+    page.should have_content "You will receive an email with instructions on how to reset your password in a few minutes."
+  end
+  scenario "wrong" do
+    visit '/'
+    click_link "Sign in"
+    click_link "Forgot your password?"
+    fill_in "user_email", with: "not_real@example.com"
+    click_button "Send me reset password instructions"
+    page.should have_content "not found"
+  end
+end
+

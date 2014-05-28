cb9f0048b6039e044e340307d36e142f2004ffc3
maxwellv <max.vance@gmail.com>
2014-05-28 11:39:46 -0500

    Added tests using email_spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,28 @@
 require "spec_helper"
+require_relative "../../app/mailers/user_mailer"
 
-describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Welcome email" do
+  include EmailSpec::Helpers
+  include EmailSpec::Matchers
+
+  before do
+    @user = User.create(email: "welcome_email_test@example.com", username: "welcome_email_test", password:"12345678", password_confirmation:"12345678")
+    @mail = UserMailer.welcome_email(@user)
+  end
+
+  it "should be from from@example.com" do
+    @mail.should deliver_from("from@example.com")
+  end
+  it "should be sent to the user's email address" do
+    @mail.should deliver_to(@user.email)
+  end
+  it "should have a subject line of 'Welcome to Squmblr'" do
+    @mail.should have_subject("Welcome to Squmblr")
+  end
+  it "should actually welcome the user to Squmblr" do
+    @mail.should have_body_text("Welcome to Squmblr, #{@user.email}!")
+  end
+  it "shouldn't have this text in it" do
+    @mail.should_not have_body_text("intentionally bad text")
+  end
 end

28e097b505b0a3d0a24f1f3abf347af154b7ce4b
Drew Moore <teloaster@yahoo.com>
2014-05-28 09:10:44 -0500

    attempted to allow users to login with username. Attempt failed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,24 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, :uniqueness => { :case_sensitive => false }
+
+  def login=(login)
+    @login = login
+  end
+  def login
+    @login || self.username || self.email
+  end
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end

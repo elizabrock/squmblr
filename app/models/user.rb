b5c2c8a789e055639b741293521a5f2d4d0a34f1
Randy Russell <ransolo@me.com>
2014-05-28 10:23:24 -0500

    last clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  attr_accessor :login
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
 

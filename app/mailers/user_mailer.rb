555dc2e70d50d0b16ede85f50da3025ea419ea2e
JamesNix <jamesfnix@gmail.com>
2014-05-29 16:18:47 -0500

    Connected mailer to layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,13 @@
 class UserMailer < ActionMailer::Base
+  # layout 'mail_layout'
   default from: "from@example.com"
 
   def welcome_email(user)
     @user = user
     @url = root_url
-    mail(to:@user.email, subject:"Welcome to Squmblr")
+    mail(to:@user.email, subject:"Welcome to Squmblr") do |format|
+      format.html { render layout: 'user_mailer' }
+      # format.text
+    end
   end
 end

5c93df7335e87dbd0f23c80c8965c3201ba79ab7
Adam Thede <adam@thedetech.com>
2014-05-29 15:48:47 -0500

    user is now able to save post as draft, view drafts, update a draft, and then publish draft :cheers:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,18 +11,57 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
-    if @post.save
-      flash[:notice] = "Your squmbl has been created"
-      redirect_to posts_path
+    if params[:commit] == 'save as draft'
+      @post.published = false
+      if @post.save
+        flash[:notice] = "Your squmbl has been saved as a draft!"
+        redirect_to user_path(current_user)
+      else
+        flash[:alert] = "Your squmbl could not be saved"
+        render :new
+      end
     else
-      flash[:alert] = "Your squmbl could not be created"
-      render :new
+      @post.published = true
+      if @post.save
+        flash[:notice] = "Your squmbl has been created"
+        redirect_to posts_path
+      else
+        flash[:alert] = "Your squmbl could not be created"
+        render :new
+      end
     end
+  end
+
+  def update
+    @post = current_user.posts.build(post_params)
+    if params[:commit] == 'save as draft'
+      @post.published = false
+      if @post.save
+        flash[:notice] = "Your squmbl has been saved as a draft!"
+        redirect_to user_path(current_user)
+      else
+        flash[:alert] = "Your squmbl could not be saved"
+        render :new
+      end
+    else
+      @post.published = true
+      if @post.save
+        flash[:notice] = "Your squmbl has been created"
+        redirect_to posts_path
+      else
+        flash[:alert] = "Your squmbl could not be created"
+        render :new
+      end
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:content, :user_id)
+    params.require(:post).permit(:content, :user_id, :published)
   end
 end
---
+++ spec/features/user_edit_post_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature "Edit posts" do
+
+  scenario "user successfully edits a post" do
+    current_user = Fabricate(:user, email: "ruby@example.com", username: "ruby")
+    login_as current_user
+    click_link "write a squmbl"
+    fill_in "Content", with: "I'm going to save this as a draft!"
+    click_button "save as draft"
+    page.should have_content "Your squmbl has been saved as a draft"
+    current_path.should == user_path(current_user)
+    page.should have_content "Drafts"
+    click_link "edit squmbl"
+    fill_in "Content", with: "This draft rocks"
+    click_button "publish squmbl"
+    page.should have_content "Your squmbl"
+  end
+end

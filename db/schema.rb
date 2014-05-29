caccc69525de534137350950c89c3ac42eb3e58c
Peder Schacht <peteschacht@gmail.com>
2014-05-29 13:46:42 -0500

    `rails g model Rating post:references user:references opinion:integer`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529155740) do
+ActiveRecord::Schema.define(version: 20140529183850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20140529155740) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "ratings", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.integer  "opinion"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ratings", ["post_id"], name: "index_ratings_on_post_id", using: :btree
+  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

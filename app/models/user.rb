1a8a78f34ee35b010628ba9541681a1eb5dd55e4
Eliza Brock <eliza@elizabrocksoftware.com>
2014-05-29 12:15:52 -0500

    Refactor find_first_by_auth_conditions

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,8 @@ class User < ActiveRecord::Base
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where("username = :value OR lower(email) = lower(:value)", value: login ).first
-    else
-      where(conditions).first
-    end
+    login = conditions.delete(:login)
+    where(conditions).where("username = :value OR lower(email) = lower(:value)", value: login ).first
   end
 
   def self.find_or_create_for_github_oauth(auth)

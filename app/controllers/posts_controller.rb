beba98406cc0f90a6ebc042cfc5bb979b14bdc57
Nat Webb <natrwebb@gmail.com>
2014-06-02 14:13:16 -0500

    refactored posts controller to tighten up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,32 +13,28 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
-    published = params[:post][:published]
     if @post.save
-      flash[:notice] = published ? "Your squmbl has been created!" : "Your squmbl has been saved as a draft!"
-      if published
-        redirect_to posts_path
+      if @post.published
+        redirect_to posts_path, :notice => "Your squmbl has been created!"
       else
-        redirect_to user_path(current_user)
+        redirect_to user_path(current_user), :notice => "Your squmbl has been saved as a draft!"
       end
     else
-      flash[:alert] = published ? "Your squmbl could not be created." : "Your squmbl could not be saved."
+      flash[:alert] = @post.published ? "Your squmbl could not be created." : "Your squmbl could not be saved."
       render :new
     end
   end
 
   def update
     @post = Post.find_by_id(params[:id])
-    published = params[:post][:published]
     if @post.update(content: params[:post][:content], published: params[:post][:published])
-      flash[:notice] = published ? "Your squmbl has been created!" : "Your squmbl has been saved as a draft!"
-      if published
-        redirect_to posts_path
+      if @post.published
+        redirect_to posts_path, :notice => "Your squmbl has been created!"
       else
-        redirect_to user_path(current_user)
+        redirect_to user_path(current_user), :notice => "Your squmbl has been saved as a draft!"
       end
     else
-      flash[:alert] = published ? "Your squmbl could not be created." : "Your squmbl could not be saved."
+      flash[:alert] = @post.published ? "Your squmbl could not be created." : "Your squmbl could not be saved."
       render :new
     end
   end
@@ -49,8 +45,8 @@ class PostsController < ApplicationController
 
   def update_published_param
     return unless params[:post]
-    is_draft = (params.delete(:commit) == "save as draft")
-    params[:post][:published] = is_draft ? false : true
+    is_published = (params.delete(:commit) == "create squmbl")
+    params[:post][:published] = is_published ? true : false
   end
 
   private

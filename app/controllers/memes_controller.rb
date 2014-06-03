188a9daed2a326d25ace90cc88c820fb069ba836
Drew Moore <teloaster@yahoo.com>
2014-06-03 14:07:11 -0500

    working on api. calls coming back, trying to upload image

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -7,9 +7,32 @@ class MemesController < ApplicationController
     @meme = Meme.find(params[:id])
   end
 
-  def self.get_meme
-    top_text = 'Yeah,'
-    bottom_text = 'Hell_Yeah'
+  def new
+    @meme = Meme.new
+  end
+
+  def create
+    @meme = current_user.memes.build(meme_params)
+    image = params[:meme][:image].path
+    get_meme(image)
+    if @meme.save
+      redirect_to memes_path, notice: "Your meme has been created"
+    else
+      render "new"
+    end
+  end
+
+  def get_meme(image)
+
+    Rails.logger.debug "\n THE MEME IS: #{@meme.inspect}\n THE IMAGE IS: #{image}\n"
+
+    upload_image(image)
+
+    Rails.logger.debug "\n BACK TO GET_MEME \n"
+
+    top_text = @meme.top_text
+    bottom_text = @meme.bottom_text
+
     base_image_title = 'Condescending%20Wonka'
     path = 'https://ronreiter-meme-generator.p.mashape.com/meme?meme=' + base_image_title + '&top=' + top_text + '&bottom=' + bottom_text + '&font=Impact&font_size=50'
     image_directory = 'app/assets/images/'
@@ -22,5 +45,30 @@ class MemesController < ApplicationController
       }
       IO.write(image_path, response.body.force_encoding("UTF-8"))
   end
-  self.get_meme
+
+  def upload_image(image)
+
+
+    # Corresponds with API from:
+    # https://www.mashape.com/ronreiter/meme-generator#!endpoint-Get-images
+
+    path = "https://ronreiter-meme-generator.p.mashape.com/images"
+    response = Unirest::post path,
+      headers: {
+        "X-Mashape-Authorization" => "o352LiepNZdiE5TAykdDyZ1RmSyc0BlG",
+        "Accept" => "application/json"
+      },
+      parameters: {
+        "image" => File.new(image, 'rb')
+      }
+
+    Rails.logger.debug "\n UPLOAD IMAGE RESPONSE: #{response.inspect} \n"
+
+  end
+
+  private
+
+  def meme_params
+    params.require(:meme).permit(:top_text, :bottom_text)
+  end
 end

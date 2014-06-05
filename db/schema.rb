5706aea33605d7e4de99128efbe619281b3c1034
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-06-05 11:49:25 -0500

    schema changed... added comments?

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529192007) do
+ActiveRecord::Schema.define(version: 20140530153046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.integer  "commentable_id",   default: 0
+    t.string   "commentable_type"
+    t.string   "title"
+    t.text     "body"
+    t.string   "subject"
+    t.integer  "user_id",          default: 0, null: false
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "posts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ancestry"
     t.string   "image"
     t.boolean  "published"
   end
+
+  add_index "posts", ["ancestry"], name: "index_posts_on_ancestry", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

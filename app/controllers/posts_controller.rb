80d9597a88f2b3a97c958ae2dc44d088d44d024b
samtes <sweldemariam@gmail.com>
2014-06-05 11:48:21 -0500

    :bird: removed the unused route and post variable in the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'comments/index'
+  get 'welcome/index'
 
   root to: "home#index"
 

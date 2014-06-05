a601de032542bdbaff9ecb152c071099f74b0cd4
Randy Russell <ransolo@me.com>
2014-06-05 00:43:13 -0500

    meme generator is working

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -16,7 +16,6 @@ class MemesController < ApplicationController
 
   def create
     @meme = current_user.memes.build(meme_params)
-
     if @meme.save
       image = params[:meme][:image].path
       uploaded_io = params[:meme][:image]
@@ -27,14 +26,9 @@ class MemesController < ApplicationController
       if image_extension != "jpg"
         image_name = image_name_url + ".jpg"
       end
-
       File.open(Rails.root.join('public', 'uploads', image_name), 'wb') do |file|
         file.write(uploaded_io.read)
       end
-
-      Rails.logger.debug "\n THIS IS THE PARAMETERS: #{params} \n"
-      Rails.logger.debug "\n THIS IS THE IMAGE NAME: #{image_name} \n"
-
       upload_image(image, image_name, image_name_url)
       redirect_to memes_path, notice: "Your memeble has been created"
     else
@@ -47,9 +41,7 @@ class MemesController < ApplicationController
     bottom_text = @meme.bottom_text.gsub(" ", "%20")
     image_name = image_name.gsub(" ", "%20")
     image_name_url = image_name.split(".")[0]
-    Rails.logger.debug "\n This is image_name: #{image_name}\n"
-
-    # path = "https://ronreiter-meme-generator.p.mashape.com/meme?meme=" + image_name + "&top=" + top_text + "&bottom=" + bottom_text + "&font=Impact&font_size=50"
+    @meme.image_path = image_name_url
     image_directory = "app/assets/images/memes/"
     image_file = @meme.id.to_s + image_name
     image_path = image_directory + image_file
@@ -57,29 +49,13 @@ class MemesController < ApplicationController
         headers: {
           "X-Mashape-Authorization" => "Kk0d6MhuSjmNjTNVMM4P1Yte1C8xsceu"
       }
-    # response = Unirest::get path,
-    #   headers: {
-    #     "X-Mashape-Authorization" => "o352LiepNZdiE5TAykdDyZ1RmSyc0BlG",
-    #     "Accept" => "application/json"
-    #   }
       IO.write(image_path, response.body.force_encoding("UTF-8"))
       @meme.meme_path = "memes/" + image_file
       Rails.logger.debug "\n GET MEME PATH IS: #{@meme.meme_path}\n"
       @meme.save
-
-
-      # Rails.logger.debug "\n GET MEME RESPONSE: #{response.inspect}\n"
-
   end
 
   def upload_image(image, image_name, image_name_url)
-    Rails.logger.debug "\n UPLOAD IMAGE, RESPONSE IS: #{image} \n"
-    #image = "app/assets/images/stratocaster3.jpg"
-
-    # Corresponds with API from:
-    # https://www.mashape.com/ronreiter/meme-generator#!endpoint-Get-images
-
-
     response = Unirest.post "https://ronreiter-meme-generator.p.mashape.com/images",
       headers: {
         "X-Mashape-Authorization" => "o352LiepNZdiE5TAykdDyZ1RmSyc0BlG",
